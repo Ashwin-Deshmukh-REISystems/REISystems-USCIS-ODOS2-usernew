f702c355962cd565988b07b317acf9ed87fb065f
aradhakrishnan-REISys <aradhakrishnan@reisystems.com>
2018-04-16 22:02:35 -0400

    Added Swagger Annotations for User

--- src/main/java/io/reisys/uscis/fads/user/api/model/User.java
+++ src/main/java/io/reisys/uscis/fads/user/api/model/User.java
@@ -2,14 +2,28 @@ package io.reisys.uscis.fads.user.api.model;
 
 import org.springframework.hateoas.ResourceSupport;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 
+@ApiModel
 public class User  extends ResourceSupport {
 
+	@ApiModelProperty(value="Id of user", required=true)
 	private String userId;
+	
+	@ApiModelProperty(value="First Name for the user", required=true)
 	private String firstName;
+	
+	@ApiModelProperty(value="Last Name for the user", required=true)
 	private String lastName;
+	
+	@ApiModelProperty(value="Email for the user", required=true)
 	private String email;
+	
+	@ApiModelProperty(value="Role name for the user", required=true)
 	private String role;
+	
+	@ApiModelProperty(value="Status of the user", required=true)
 	private String status;
 	
 	public String getUserId() {
