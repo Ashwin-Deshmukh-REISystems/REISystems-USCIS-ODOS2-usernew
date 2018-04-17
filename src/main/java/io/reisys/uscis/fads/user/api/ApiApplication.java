50fbf8f374931d9817b468404110c0168e80404f
aradhakrishnan-REISys <aradhakrishnan@reisystems.com>
2018-04-17 12:02:27 -0400

    Disabling Spring security for now

--- src/main/java/io/reisys/uscis/fads/user/api/ApiApplication.java
+++ src/main/java/io/reisys/uscis/fads/user/api/ApiApplication.java
@@ -7,12 +7,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.core.Ordered;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+//import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-@EnableResourceServer
+//@EnableResourceServer
 @SpringBootApplication
 
 public class ApiApplication {
